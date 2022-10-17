04135e5acba8167c9549e9c603cde373f4241e48
madrimovDev <madrimov5014@gmail.com>
2022-10-17 12:14:30 +0500

    refactor authReducer

--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -1,39 +1,46 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createReducer, PayloadAction } from '@reduxjs/toolkit'
 import { loginAction, logoutAction, verifyAction } from '../actions'
 import { InitialState } from '@store/types/types'
 import { UserMappedType } from '@mapper/authResponseMapper'
+import { errorHandler } from '@utils'
 
 const initialState: InitialState<UserMappedType | null> = {
 	status: 'DEFAULT',
 	data: null,
-	message: '',
-	errorMessage: null,
-	error: false
-
+	response: {
+		error: false,
+		errorMessage: ''
+	}
 }
 
 const authReducer = createReducer(initialState, builder => {
 	builder
 	.addCase(loginAction.pending, ( state ) => {
 		state.status = 'PENDING'
+		state.response = errorHandler(false)
 	})
 	.addCase(loginAction.fulfilled, ( state, action ) => {
 		state.status = 'FULFILLED'
 		state.data = action.payload
+		state.response = errorHandler(false)
 	})
-	.addCase(loginAction.rejected, ( state, action ) => {
+	.addCase(loginAction.rejected, ( state, action: PayloadAction<any, any> ) => {
 		state.status = 'REJECTED'
 		state.data = null
+		state.response = errorHandler(true, action.payload.message)
 	}).addCase(verifyAction.pending, ( state ) => {
 		state.status = 'PENDING'
+		state.response = errorHandler(false)
 	})
 	.addCase(verifyAction.fulfilled, ( state, action ) => {
 		state.status = 'FULFILLED'
 		state.data = action.payload
+		state.response = errorHandler(false)
 	})
-	.addCase(verifyAction.rejected, ( state, action ) => {
+	.addCase(verifyAction.rejected, ( state, action: PayloadAction<any, any> ) => {
 		state.status = 'REJECTED'
 		state.data = null
+		state.response = errorHandler(true, action.payload.message)
 	})
 	.addCase(logoutAction, ( state, action ) => {
 		state.status = 'DEFAULT'
