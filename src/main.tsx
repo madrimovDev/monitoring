ee4b33a41257a1c08430f7e0a1db597253036b15
madrimovDev <madrimov5014@gmail.com>
2022-10-21 15:46:12 +0500

    reload button for testing

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { store } from '@store'
 import { verifyAction } from '@store/actions'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from '@routes'
+import { Button } from 'antd'
+import { ReloadOutlined } from '@ant-design/icons'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
@@ -18,6 +20,13 @@ const app = (
 		<SidebarProvider>
 			<RouterProvider router={routes} />
 		</SidebarProvider>
+		<Button type={'primary'} danger shape={'round'} size={'large'} style={{
+			position: 'fixed',
+			bottom: '20px',
+			right: '20px'
+		}} onClick={() => {
+			window.location.reload()
+		}} icon={<ReloadOutlined />} children={'Reload Page'} />
 	</Provider>
 )
 
